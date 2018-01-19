7864ef6047e2e225a822838213e9d8840de97329
Waquid Valiya Peedikakkal <waquidvp@gmail.com>
2018-01-19 22:35:37 +0000

    Added react-native-interactable and created rough version of card stack

--- client/src/Home/Cards.js
+++ client/src/Home/Cards.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react';
+import { Animated, Dimensions, StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
+import Interactable from 'react-native-interactable';
+
+const Screen = Dimensions.get('window');
 
 const MainContainer = styled.View`
   flex: 1;
+  justify-content: center;
+  align-items: center;
 `;
+
+const Card = styled.View`
+  flex: 1;
+  background-color: white;
+  margin-top: 16px;
+  margin-bottom: 16px;
+  border-radius: 15;
+  elevation: 6;
+  width: ${props => props.width - 32};
+`;
+
+const SecondCard = styled.View`
+  flex: 1;
+  background-color: white;
+  border-radius: 15;
+  elevation: 4;
+  width: ${props => props.width - 48};
+  margin-bottom: 12px;
+  margin-top: 20px;
+`;
+
+const AnimatedCard = Animated.createAnimatedComponent(Card);
+
+const styles = StyleSheet.create({
+  card: {
+    position: 'absolute',
+    height: '100%',
+    width: Screen.width + 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    elevation: 6,
+  },
+});
 
 class Cards extends Component {
   static navigationOptions = {
@@ -11,9 +50,39 @@ class Cards extends Component {
     swipeEnabled: false,
   };
 
+  constructor() {
+    super();
+
+    this.deltaX = new Animated.Value(0);
+  }
+
   state = {};
   render() {
-    return <MainContainer />;
+    return (
+      <MainContainer>
+        <Interactable.View
+          style={styles.card}
+          snapPoints={[{ x: 390 }, { x: 0 }, { x: -390 }]}
+          animatedValueX={this.deltaX}
+          horizontalOnly
+        >
+          <AnimatedCard
+            width={Screen.width}
+            style={{
+              transform: [
+                {
+                  rotate: this.deltaX.interpolate({
+                    inputRange: [-250, 0, 250],
+                    outputRange: ['-5deg', '0deg', '5deg'],
+                  }),
+                },
+              ],
+            }}
+          />
+        </Interactable.View>
+        <SecondCard width={Screen.width} />
+      </MainContainer>
+    );
   }
 }
 
